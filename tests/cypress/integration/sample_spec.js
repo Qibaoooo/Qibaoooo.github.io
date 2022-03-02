a639234e0448c790a001463002543abd9b526f8d
zheng <luozhengiw@gmail.com>
2022-03-02 20:44:49 +0800

    simple test

--- tests/cypress/integration/sample_spec.js
+++ tests/cypress/integration/sample_spec.js
@@ -1,5 +1,16 @@
+import DataProvider from './utils/DataProvider'
+
 describe('My First Test', () => {
     it('Visits the Site', () => {
-      cy.visit('https://qibaoooo.github.io/templates/index.html')
+      cy.visit('/')
+
+      cy.pause()
+
+      cy.contains(DataProvider.STR_LETS_JIO).click()
+      
+      cy.pause()
+
+      cy.get('legend')
+        .should('be.visible')
     })
   })
---
+++ tests/cypress/integration/utils/DataProvider.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze (
+    {
+        STR_LETS_JIO : "Let's Jio",
+        STR_SESSION_DETAILS : "Session Details",
+    }
+);
