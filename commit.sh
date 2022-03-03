379a843da710d4c954e9809690dc57b7180555bb
zheng <luozhengiw@gmail.com>
2022-03-03 19:28:08 +0800

    script [skip ci]

--- commit.sh
+++ commit.sh
@@ -1,3 +1,14 @@
+set -xe
+
+need_ci=${1:-false}
+
 git add .
-git commit -m 'script [skip ci]'
+
+if $need_ci
+then
+  git commit -m 'script'
+else
+  git commit -m 'script [skip ci]'
+fi
+
 git push
