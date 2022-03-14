36b437eb6d302a7bca7e314102c083059fdaafe5
Zheng Luo <zheng.luo@shopee.com>
2022-03-15 00:05:36 +0800

    v2 init

--- README.md
+++
@@ -1,4 +0,0 @@
-## Qibaoooo.github.io
-> ClimbJio
-
-**A light weight web app for my friends to have climb sessions together.**
--- tests/cypress/integration/utils/DataProvider.js
+++
@@ -1,6 +0,0 @@
-module.exports = Object.freeze (
-    {
-        STR_LETS_JIO : "Let's Jio",
-        STR_SESSION_DETAILS : "Session Details",
-    }
-);
