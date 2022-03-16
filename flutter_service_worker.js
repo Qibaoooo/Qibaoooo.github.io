7896b65cf3ce90df01d5ed47d5dd6c67b3722341
zheng <luozhengiw@gmail.com>
2022-03-17 01:50:52 +0800

    update [skip ci]

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -136,9 +136,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "1d50ac0f96a82d650db6eaae78d3bae3",
-"/": "1d50ac0f96a82d650db6eaae78d3bae3",
-"main.dart.js": "d19a26bc288860bede0b97a66543f32f",
+"index.html": "dca3d89055203c47fe6c11ec8a21d166",
+"/": "dca3d89055203c47fe6c11ec8a21d166",
+"main.dart.js": "7a2e55fa8ffc53ccca809445f0946a99",
 "manifest.json": "9bdf0f97a0f7bfedb4ef32dd30346a66",
 "version.json": "6bc521c4f5a00a15c5f344f0d9e5942a"
 };
