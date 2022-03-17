3e94b719b7fb44f2ddbfd849eb00debe0f32c14b
zheng <luozhengiw@gmail.com>
2022-03-17 10:32:49 +0800

    update [skip ci]

--- dev/release_web.sh
+++ dev/release_web.sh
@@ -2,18 +2,18 @@
 
 set -x
 
-echo '=========build========='
 cd C:\\Source\\climbjio\\dev
-
 flutter build web
+echo -e '===>flutter build web DONE\n\n'
+
+rsync -av --exclude='index.html' build/web/ ../.
+echo -e '===>rsync DONE\n\n'
+
+set +x
+
+echo 'check please. enter to exit'
+read anything -x
 
 # cd build/web/
 # python -m http.server 8083 &
 # cd ../../
-
-echo '=========rsync========='
-rsync -av --exclude='index.html' build/web/ ../.
-
-set +x
-echo 'check please. enter to exit'
-read anything
