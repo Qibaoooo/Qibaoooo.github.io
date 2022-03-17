07fc3fdca71ecaca1f1914cb51fc8ed59830aedd
zheng <luozhengiw@gmail.com>
2022-03-17 10:41:47 +0800

    update [skip ci]

--- dev/lib/sessionForm.dart
+++ dev/lib/sessionForm.dart
@@ -44,7 +44,6 @@ class _SessionFormState extends State<SessionForm> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     apiGetArchivedSession().then((value) {
       setState(() {
